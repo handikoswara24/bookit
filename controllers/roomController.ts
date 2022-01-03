a865dc3fffb2f7f0f0ee8f70a8ccf2c6e0855883
handikoswara24 <handi.koswara24@gmail.com>
2022-01-03 21:04:25 +0700

    filter and pagination

--- controllers/roomController.ts
+++ controllers/roomController.ts
@@ -2,13 +2,24 @@ import { NextApiHandler, NextApiRequest, NextApiResponse } from "next"
 import Room from "../models/room";
 import ErrorHandler from "../utils/errorHandler";
 import catchAsyncErrors from "../middlewares/catchAsyncErrors";
+import APIFeatures from "../utils/apiFeatures";
 
 const allRooms = catchAsyncErrors(async (req: NextApiRequest, res: NextApiResponse) => {
-    const rooms = await Room.find({});
+    const resPerPage = 4;
+    const roomsCount = await Room.countDocuments();
+    const apiFeatures = new APIFeatures(Room.find(), req.query).search().filter();
+
+    let rooms = await apiFeatures.query;
+    const filteredRoomsCount = rooms.length;
+
+    apiFeatures.pagination(resPerPage);
+    rooms = await apiFeatures.query.clone();
 
     res.status(200).json({
         success: true,
-        count: rooms.length,
+        roomsCount,
+        resPerPage,
+        filteredRoomsCount,
         rooms
     })
 })
