9ed2e65fbc0b59dd644a023d80e4cbfce8de9c2d
handikoswara <handi.koswara24@gmail.com>
2022-01-09 19:41:13 +0700

    room details

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,16 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
     return (
         <nav className="navbar row justify-content-center sticky-top">
-            <div className="container">
+            <div className="container" style={{ maxWidth: "1320px" }}>
                 <div className="col-3 p-0">
                     <div className="navbar-brand">
-                        <img style={{cursor: "pointer"}} src="./images/bookit_logo.png" alt="BookIT" />
+                        <Link href="/">
+                            <img style={{ cursor: "pointer" }} src="/images/bookit_logo.png" alt="BookIT" />
+                        </Link>
                     </div>
                 </div>
 
-                <div className="col-3 mt-3 mt-md-0 text-center">
+                <div className="col-3 mt-3 mt-md-0 text-end">
                     <a className="btn btn-danger px-4 text-white login-header-btn float-right">Login</a>
                 </div>
             </div>
